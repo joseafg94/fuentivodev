255f19b3f6dc2ec33466259d596034be195a4176
Jose Fuentes <jose.fu0409@gmail.com>
2026-08-05 02:24:15 -0500

    feat: add PWA web app manifest configuration

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,19 +14,19 @@ export default function manifest(): MetadataRoute.Manifest {
     lang: "es",
     icons: [
       {
-        src: "/icons/appico.png",
+        src: "/icons/pwa-192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icons/appico.png",
+        src: "/icons/pwa-512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icons/appico.png",
+        src: "/icons/pwa-maskable-512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
